47f42b88eed4998e5a3ac6b0ef200e78b4b7cd34
garrett waterman <waterboy101010@hotmail.com>
2021-01-18 16:09:43 -0600

    added skeleton classes

--- CIS353GroupB/CIS353GroupB/Program.cs
+++ CIS353GroupB/CIS353GroupB/Program.cs
@@ -2,6 +2,9 @@
 
 namespace CIS353GroupB
 {
+    // team class located in TeamClass.cs
+    // player class located in PlayerClass.cs
+
     class Program
     {
         static void Main()
---
+++ CIS353GroupB/CIS353GroupB/TeamClass.cs
@@ -0,0 +1,12 @@
+﻿// Garrett Waterman
+// 1.18.2021
+// Class to represent a golf team with 4 players and a scoring rank
+
+using System;
+
+namespace CIS353GroupB
+{
+	public class Team
+	{
+	}
+}
