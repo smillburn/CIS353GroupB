20e75de11b93b436f50e31a0bd54d7bf796dedab
Henry Flores <HenryR.Flores@yahoo.com>
2021-02-15 15:51:39 -0700

    Added Class and logic to autofill CreateTeams Form, clear the form, and exit to main

--- CIS353GroupB/CreateTeams_Form.cs
+++ CIS353GroupB/CreateTeams_Form.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace CIS353GroupB
 {
@@ -20,7 +21,44 @@ namespace CIS353GroupB
             /*Fill form with random data. Need to make a random class with arrays of first names,
              * last names, handicaps, last score, and team score and randomly select one for entry. 
              * Maybe use IComparable to sort players by score and use array values to display their rank.
-             * Henry Flores: 12Feb21 */
+             * Henry Flores: 12Feb21
+             * 
+             * 
+             * Created class with list of names and numbers to autofill the textboxes,
+             * I used a regex to match textbox name for the correct field and used the methods in new class
+             * to auto fill the appropiate textbox. Still would like to see some kind of way to calculate
+             * the score based on the random handicap and score that was generated. Maybe this textbox shouldn't
+             * exist in the CreateTeams Form?
+             * Henry Flroes: 15Feb21 */
+
+            foreach (Control control in this.Controls)
+            {
+                if (Regex.Match(control.Name, "^P[0-9]_Fname.*").Success)
+                {
+                    control.Text = RandGenTeam.GetFirstName();
+                }
+            }
+            foreach (Control control in this.Controls)
+            {
+                if (Regex.Match(control.Name, "^P[0-9]_Lname.*").Success)
+                {
+                    control.Text = RandGenTeam.GetLastName();
+                }
+            }
+            foreach (Control control in this.Controls)
+            {
+                if (Regex.Match(control.Name, "^P[0-9]_hCap.*").Success)
+                {
+                    control.Text = RandGenTeam.GetHandicap();
+                }
+            }
+            foreach (Control control in this.Controls)
+            {
+                if (Regex.Match(control.Name, "^P[0-9]_lastGame.*").Success)
+                {
+                    control.Text = RandGenTeam.GetScore();
+                }
+            }
         }
 
         private void Create_btn_Click(object sender, EventArgs e)
@@ -33,13 +71,22 @@ namespace CIS353GroupB
         private void Clear_btn_Click(object sender, EventArgs e)
         {
             /*Clear all of the text boxes
-             * Henry Flores: 12Feb21 */
+             * Henry Flores: 12Feb21
+             * 
+             * Added the logic to clear all of the textbox fields
+             * Henry Flores: 15Feb21 */
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox)
+                    control.Text = null;
+            }
         }
 
         private void Main_btn_Click(object sender, EventArgs e)
         {
             /*Return to main menu (Title Form)
              * Henry Flores: 12Feb21 */
+            this.Close();
         }
     }
 }
---
+++ CIS353GroupB/RandGenTeam.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIS353GroupB
+{
+    class RandGenTeam
+    {
+        static string[] randFnames = {"Glennis", "Maynard", "Branda", "Rosalinda", "Sebrina", "Ronda",
+            "Lurlene", "Rachelle", "Petronila", "Carmen", "Guadalupe", "Teisha",
+            "Merlin", "Raeann", "Marvella", "Carol", "Benton", "Gita", "Blanche",
+            "Claris", "Abdul", "Jamison", "Dominique", "Theo", "Mike", "Markus",
+            "Clint", "Zachery", "Stacy", "Cyrus", "Charlie", "Kip", "Ismael",
+            "Devin", "Hollis", "Earnest", "Gustavo", "Alton", "Delbert", "Tod" };
+        static string[] randLnames = {"Taubman", "Wedderburn", "Gash", "Nicol", "Seaton", "Barrientez",
+            "Kaczmarek", "Beesley", "Biondo", "Yearout", "Stoll", "Mills", "Redel",
+            "Gilson", "Gilroy", "Pricer", "Clemens", "Montalto", "Ratzlaff", "Sorrell",
+            "Kuck", "Hamner", "Guion", "Vail", "Scharf", "Weckerly", "Maharaj", "Howell",
+            "Ambler", "Figueras", "Kolstad", "Coon", "Chavera", "Mcgonigle", "Keese",
+            "Sheard", "Lundberg", "Loughlin", "Hynson", "Fleckenstein"};
+        static int[] handicap = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        static int[] lastGameScore = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+            81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
+        static Random rand = new Random();
+        static public string GetFirstName()
+        {
+            return randFnames[rand.Next(0, randFnames.Length - 1)];
+        }
+        static public string GetLastName()
+        {
+            return randLnames[rand.Next(0, randLnames.Length - 1)];
+        }
+        static public string GetHandicap()
+        {
+            return handicap[rand.Next(0, handicap.Length - 1)].ToString();
+        }
+        static public string GetScore()
+        {
+            return lastGameScore[rand.Next(0, lastGameScore.Length - 1)].ToString();
+        }
+    }
+}
