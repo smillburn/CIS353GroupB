8c2a4bd9652148b5e0ee14cc869a7ba440cd949f
Henry Flores <HenryR.Flores@yahoo.com>
2021-03-02 22:38:56 -0700

    Added random team names to include Fuzzy Wuzzy :-)

--- CIS353GroupB/RandGenTeam.cs
+++ CIS353GroupB/RandGenTeam.cs
@@ -21,22 +21,31 @@ namespace CIS353GroupB
         static int[] handicap = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         static int[] lastGameScore = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
             81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
+        static string[] randTnames = { "Fuzzy Wuzzy", "Retro Ducks", "Gecko Stars", "Dream Busters",
+            "Killer Executioners", "Carnivore Kickers", "Night Flyers", "Muffin Bombers", "Mighty Mavericks",
+            "Smokin Lunatics", "Aero Bullets", "Militant Wasps", "Scorpion Sonics", "Crimson Wasps",
+            "Wild Predators", "Demolition Mashers", "Zulu Falcons", "Reptile Fighters", "Skull Crunchers",
+            "Silent Tanks", "Banzai Stars", "Spinning Gangstaz", "Flash Dragons", "Eccentric Sonics"};
         static Random rand = new Random();
         static public string GetFirstName()
         {
-            return randFnames[rand.Next(0, randFnames.Length - 1)];
+            return randFnames[rand.Next(0, randFnames.Length)];
         }
         static public string GetLastName()
         {
-            return randLnames[rand.Next(0, randLnames.Length - 1)];
+            return randLnames[rand.Next(0, randLnames.Length)];
         }
         static public string GetHandicap()
         {
-            return handicap[rand.Next(0, handicap.Length - 1)].ToString();
+            return handicap[rand.Next(0, handicap.Length)].ToString();
         }
         static public string GetScore()
         {
-            return lastGameScore[rand.Next(0, lastGameScore.Length - 1)].ToString();
+            return lastGameScore[rand.Next(0, lastGameScore.Length)].ToString();
+        }
+        static public string GetTeamName()
+        {
+            return randTnames[rand.Next(0, lastGameScore.Length)].ToString();
         }
         static public Player getPlayer()
         {
