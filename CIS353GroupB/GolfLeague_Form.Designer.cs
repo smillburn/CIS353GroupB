91d476c5e3fe83cbcb19059a05134265f1a3514d
Henry Flores <HenryR.Flores@yahoo.com>
2021-03-03 19:56:01 -0700

    Disable the rank textbox

--- CIS353GroupB/GolfLeague_Form.Designer.cs
+++ CIS353GroupB/GolfLeague_Form.Designer.cs
@@ -194,6 +194,7 @@ namespace CIS353GroupB
             // 
             // txtG4Rank
             // 
+            this.txtG4Rank.Enabled = false;
             this.txtG4Rank.Location = new System.Drawing.Point(442, 216);
             this.txtG4Rank.Name = "txtG4Rank";
             this.txtG4Rank.Size = new System.Drawing.Size(100, 20);
@@ -232,6 +233,7 @@ namespace CIS353GroupB
             // 
             // txtG3Rank
             // 
+            this.txtG3Rank.Enabled = false;
             this.txtG3Rank.Location = new System.Drawing.Point(442, 190);
             this.txtG3Rank.Name = "txtG3Rank";
             this.txtG3Rank.Size = new System.Drawing.Size(100, 20);
@@ -270,6 +272,7 @@ namespace CIS353GroupB
             // 
             // txtG2Rank
             // 
+            this.txtG2Rank.Enabled = false;
             this.txtG2Rank.Location = new System.Drawing.Point(442, 164);
             this.txtG2Rank.Name = "txtG2Rank";
             this.txtG2Rank.Size = new System.Drawing.Size(100, 20);
@@ -308,6 +311,7 @@ namespace CIS353GroupB
             // 
             // txtG1Rank
             // 
+            this.txtG1Rank.Enabled = false;
             this.txtG1Rank.Location = new System.Drawing.Point(442, 138);
             this.txtG1Rank.Name = "txtG1Rank";
             this.txtG1Rank.Size = new System.Drawing.Size(100, 20);
